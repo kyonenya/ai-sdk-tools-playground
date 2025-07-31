cf36e2f7a18830a8a8a920b9544eb5acc157535f
kyonenya <kyon.musica@gmail.com>
2025-07-31 21:35:32 +0900

    feat: cityMapという背景情報をプロンプトに明示的に渡す

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import { openai } from "@ai-sdk/openai";
 import { streamText, tool, CoreMessage } from "ai";
 import { z } from "zod";
+import cityMap from "./cityMap.json";
 
 export const maxDuration = 30;
 
@@ -13,19 +14,21 @@ export async function POST(req: Request) {
      * マルチステップ呼び出しを有効化
      * @see https://ai-sdk.dev/docs/ai-sdk-core/tools-and-tool-calling#multi-step-calls-using-maxsteps
      */
-    maxSteps: 5,
+    maxSteps: 2,
     messages: [
       {
         role: "system",
         content:
-          "天気APIの生データを受け取ったら、日本語でわかりやすく要約して返してください。",
+          `weather ツールは、引数に一次細分区域IDを受け取り、天気取得APIを叩いて生のJSONを返します。あなたは、入力された場所から最も近い地域を下記の「一時細分区域ID一覧」から選んで、そのIDを指定して weather ツールを呼んでください。あなたはその後、天気APIから返ってきたJSONを日本語でわかりやすく要約してください。
+          --- 一時細分区域IDの一覧 ---
+          ${JSON.stringify(cityMap)}`.trim(),
       },
       ...messages,
     ],
     tools: {
       weather: tool({
         description:
-          '日本の現在の天気を返します。引数`location`には一次細分区域（cityタグ）のID（例: 東京→"130010"）を指定してください。',
+          "日本の現在の天気を返します。引数 location には一次細分区域のIDを指定してください。",
         parameters: z.object({
           location: z.string().describe("The location to get the weather for"),
         }),
