c335d4f6f7a149cf9178aac1de3518a203a12dc8
kyonenya <kyon.musica@gmail.com>
2025-08-10 03:01:49 +0900

    refactor: 非制御コンポーネントに

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { useChat } from "@ai-sdk/react";
-import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 export default function Chat() {
   const { messages, sendMessage } = useChat();
-  const [input, setInput] = useState("");
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch gap-4">
@@ -43,17 +41,19 @@ export default function Chat() {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          const text = input.trim();
+          const form = e.currentTarget as HTMLFormElement;
+          const formData = new FormData(form);
+          const text =
+            (formData.get("user-input") as string | null)?.trim() ?? "";
           if (text.length === 0) return;
           sendMessage({ text });
-          setInput("");
+          form.reset();
         }}
       >
         <input
+          name="user-input"
           className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
-          value={input}
           placeholder="Say something..."
-          onChange={(e) => setInput(e.target.value)}
         />
       </form>
     </div>
